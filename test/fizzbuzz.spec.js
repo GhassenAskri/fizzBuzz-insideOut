4b633c9c4b230cbad2c8a7d0c22b3baa0e465b3b
lenovo 3 <lenovo 3>
2020-10-14 15:29:13 +0200

    Refacto:renaming tests

--- test/fizzbuzz.spec.js
+++ test/fizzbuzz.spec.js
@@ -2,42 +2,42 @@ import {convertOneNumberToString,printFizzBuzzString,convertNumbersToStrings} fr
 
 describe('numbers that are not multiple ' +
     'of 3 or 5 or both', () => {
-    test('When I fizzbuzz 1, i get back a string representing' +
+    test('When I convert 1 to a string, i get back a string representing' +
         'it', () => {
         expect(convertOneNumberToString(1)).toStrictEqual('1')
     });
-    test('When I fizzbuzz 2, i get back a string representing' +
+    test('When I convert 2 to a string, i get back a string representing' +
         'it', () => {
         expect(convertOneNumberToString(2)).toStrictEqual('2')
     });
-    test('When I fizzbuzz 4, i get back a string representing' +
+    test('When I convert 4 to a string, i get back a string representing' +
         'it', () => {
         expect(convertOneNumberToString(4)).toStrictEqual('4')
     });
 });
 describe('numbers that are multiple of 3', () => {
-    test('When I fizzbuzz 3, i get back fizz' +
+    test('When I convert 3 to a string , i get back fizz' +
         'it', () => {
         expect(convertOneNumberToString(3)).toStrictEqual('fizz')
     });
-    test('When I fizzbuzz 6, i get back a fizz' +
+    test('When I convert 6 to a string, i get back a fizz' +
         'it', () => {
         expect(convertOneNumberToString(6)).toStrictEqual('fizz')
     });
-    test('When I fizzbuzz 9, i get back a fizz' +
+    test('When I convert 9 to a string, i get back a fizz' +
         'it', () => {
         expect(convertOneNumberToString(9)).toStrictEqual('fizz')
     });
 });
 
 describe('numbers that are multiple of 5', () => {
-    test('When I fizzbuzz 5, i get back buzz', () => {
+    test('When I convert 5 to a string, i get back buzz', () => {
         expect(convertOneNumberToString(5)).toStrictEqual('buzz')
     });
 });
 
 describe('numbers that are multiple of 3 and 5', () => {
-    test('When I fizzbuzz 15, i get back fizzbuzz' +
+    test('When I convert 15 to a string, i get back fizzbuzz' +
         'it', () => {
         expect(convertOneNumberToString(15)).toStrictEqual('fizzbuzz')
     });
@@ -49,7 +49,7 @@ describe('array of numbers that contains ' +
     ', multiples of 3 ' +
     ', multiples of 5 ' +
     'and multiples of them both', () => {
-    test('When I calculate fizzbuzz for [1,2,15,9,10,100], i get back 1 2 fizzbuzz fizz buzz buzz', () => {
+    test('When I print a fizzBuzzString from an array equal to [1,2,15,9,10,100], i get back 1 2 fizzbuzz fizz buzz buzz', () => {
         const arrayNumbers = [1,2,15,9,10,100]
         const numbersAsStrings = convertNumbersToStrings(arrayNumbers)
 
